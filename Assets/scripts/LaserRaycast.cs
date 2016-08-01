739f5eed0e36888b1b4ef434705687c6ab2ad126
Rachel Clayton <rachelclayton95@gmail.com>
2016-08-01 14:20:52 -0400

    dumb Enemy v1

---
+++ Assets/scripts/LaserRaycast.cs
@@ -0,0 +1,35 @@
+﻿//using UnityEngine;
+//using System.Collections;
+//
+//public class LaserRaycast : MonoBehaviour {
+//
+//    public GameObject laserbeamObject;
+//
+//	void Start () {
+//		InvokeRepeating (LaserBeam, 1, 2);
+//
+//	}
+//
+//	// Update is called once per frame
+//	void Update () {
+//        transform.Rotate (0f, 0f, -Input.GetAxis("Horizontal") * Time.deltaTime * 90f);
+//
+//	}
+////
+////		void LaserBeam () {
+////            laserbeamObject.SetActive(true);
+////
+////            RaycastHit2D raycastHit = Physics2D.Raycast( transform.position, transform.up, 50f );
+////            //checks results of raycast
+////            if ( raycastHit.collider != null )
+////            {
+////                Destroy(raycastHit.collider.gameObject); 
+////            }
+//        
+//
+////        else
+////        {
+////            laserbeamObject.SetActive(false);
+////        }
+//	}
+//}
