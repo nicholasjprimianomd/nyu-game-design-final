e7e8e2a89a90045f13fd02000ede9d7a194ff5b9
Olivia Hu <olivia@172-16-31-113.DYNAPOOL.NYU.EDU>
2016-08-01 14:25:23 -0400

    olivia's changes

---
+++ Assets/ArrowMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowMove : MonoBehaviour {
+
+	GameObject player;
+	Vector2 arrowMoves = new Vector2 (0f, 0f);
+	Rigidbody2D myRigidbody; // Opens a Rigidbody for the bullet
+	public int arrowSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D> ();
+		player = GameObject.FindWithTag("Player");
+
+		Vector3 playerPosition = player.transform.position;
+		transform.position = playerPosition;
+		Vector3 playerDirection = player.transform.up;
+		arrowMoves = playerDirection;
+	}
+
+	void FixedUpdate () {
+		myRigidbody.velocity = arrowMoves * arrowSpeed;
+	}
+
+}
